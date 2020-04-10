ad955398a52eaf94064247d2ca727a92a54340b8
sanfira <60214461+sanfira@users.noreply.github.com>
2020-04-10 18:19:02 +0700

    Kode membaca 2 baris pertama file
    Input berupa nama file saja
    Jika ditemukkan file maka diminta input ulang
    Jika telah ditemukan menyimpan baris pertama sebagai jumlah baris, baris kedua sebagai jumlah kolom

--- ExternalFile.c
+++ ExternalFile.c
@@ -1,19 +1,36 @@
 
 char ReadFile(char *GOL){
-    //open
-    //fgets buat ambil jumlah baris -->ubah ke int
-    //fgets ambil jumlah kolom --> ubah ke int
-    //bikin array[n][m] 
+    FILE *seed;
+    char filename[10]="";
+    char tipe[]=".txt";
+    char line[2][5];//jumlah baris dan kolom
+    int baris, kolom,i;
 
-   /* define ROW 5
-    #define COLUMN 10
-    main()
-    {
-    unsigned char **p = NULL, colum = 0;
-    p = malloc ( ROW * sizeof ( unsigned char *) );
-    for (;colum< ROW; ++colum )
-    {
-        p[colum] = malloc (COLUMN * sizeof (unsigned char  ));
+
+    printf("Input Nama File:");
+    scanf("%s", &filename);
+    strcat(filename, tipe);
+    printf("%s\n", filename);
+    seed = fopen(filename, "r");
+    
+    while(seed == NULL){
+        printf("Error Opening File\n");
+        printf("Input Nama File:");
+        scanf("%s", &filename);
+        strcat(filename, tipe);
+        printf("%s", filename);
+        seed = fopen(filename, "r");
+    } 
+
+    i=0;
+    while(i<2){
+        fgets(line[i], 5, seed);
+        i++;
     }
-    }*/
+    baris = atoi(line[0]);
+    kolom = atoi(line[1]);
+    printf("%d\n", baris);
+    printf("%d\n", kolom);
+    
+    fclose(seed);
 }
