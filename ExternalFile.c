d06bbcbe4c077e2cffff4b8c33c96e91164f06e9
sanfira <60214461+sanfira@users.noreply.github.com>
2020-04-10 19:22:22 +0700

    Memasukkan seed kedalam array
    gol merupakan array of pointer

--- ExternalFile.c
+++ ExternalFile.c
@@ -3,8 +3,10 @@ char ReadFile(char *GOL){
     FILE *seed;
     char filename[10]="";
     char tipe[]=".txt";
+    char newline;
     char line[2][5];//jumlah baris dan kolom
-    int baris, kolom,i;
+    int baris, kolom,i,j;
+    unsigned char **GOL =NULL;
 
 
     printf("Input Nama File:");
@@ -31,6 +33,25 @@ char ReadFile(char *GOL){
     kolom = atoi(line[1]);
     printf("%d\n", baris);
     printf("%d\n", kolom);
+    
+    GOL = malloc(baris*sizeof(unsigned char*));
+    for(i=0; i<baris; i++){
+        GOL[i]=malloc(kolom*sizeof(unsigned char));
+    }
+
+    for(i=0;i<baris;i++){
+        for(j=0; j<kolom;j++){
+            GOL[i][j]=fgetc(seed);
+        }
+        newline=fgetc(seed);
+    }
+
+    for(i=0;i<baris;i++){
+        for(j=0; j<kolom;j++){
+            printf("%c", GOL[i][j]);
+        }
+        printf("\n");
+    }
     
     fclose(seed);
 }
