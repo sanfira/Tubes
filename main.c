0a6b3dbd8742fd7eacf1797a67d2307abebe19c8
sanfira <60214461+sanfira@users.noreply.github.com>
2020-04-15 17:49:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
  * MODUL 8 – TUGAS BESAR
  * Kelompok : 1
- * Hari dan Tanggal : Kamis, 9 April 2020
+ * Hari dan Tanggal : Rabu, 15 April 2020
  * Asisten (NIM) : Berry (13216027)
  * Nama File : main.c
  * Deskripsi :
