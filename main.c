5c4666a921d8dffb7ad6559cc58ef5a4e7681c92
sanfira <60214461+sanfira@users.noreply.github.com>
2020-04-15 17:43:45 +0700

    Adding read file programm
    Menambahkan readfile

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- * MODUL 8 – TUGAS BESAR
+ * MODUL 8 â€“ TUGAS BESAR
  * Kelompok : 1
  * Hari dan Tanggal : Kamis, 9 April 2020
  * Asisten (NIM) : Berry (13216027)
@@ -27,12 +27,64 @@ int main(){
     bool load=true;
     bool out=false;
     int ins;
+    FILE *seed;
+    char filename[10]="";
+    char tipe[]=".txt";
+    char newline;
+    char line[2][5];//jumlah baris dan kolom
+    int baris, kolom,i,j;
+    unsigned char **GOL =NULL;
 
     printf("\nWelcome to\n");
     printf("GAME OF LIFE by E-1\n\n");
     printf("--insert deskripsi program here--\n\n");
 
     while(load==true){
+    printf("Input File Name:");
+    scanf("%s", &filename);
+    strcat(filename, tipe);
+    printf("%s\n", filename);
+    seed = fopen(filename, "r");
+    
+    while(seed == NULL){
+        printf("Error Opening File\n");
+        printf("Input File Name:");
+        scanf("%s", &filename);
+        strcat(filename, tipe);
+        printf("%s", filename);
+        seed = fopen(filename, "r");
+    } 
+
+    i=0;
+    while(i<2){
+        fgets(line[i], 5, seed);
+        i++;
+    }
+    baris = atoi(line[0]);
+    kolom = atoi(line[1]);
+    printf("%d\n", baris);
+    printf("%d\n", kolom);
+    
+    GOL = malloc(baris*sizeof(unsigned char*));
+    for(i=0; i<baris; i++){
+        GOL[i]=malloc(kolom*sizeof(unsigned char));
+    }
+
+    for(i=0;i<baris;i++){
+        for(j=0; j<kolom;j++){
+            GOL[i][j]=fgetc(seed);
+        }
+        newline=fgetc(seed);
+    }
+
+    for(i=0;i<baris;i++){
+        for(j=0; j<kolom;j++){
+            printf("%c", GOL[i][j]);
+        }
+        printf("\n");
+    }
+    fclose(seed);
+        
     printf("\nloading--\n"); //seed file read
     quit=false;
     out=false;
