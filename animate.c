b60bd20fd8b032490f248bdaa56f238024a1d1cf
rifda123 <63699475+rifda123@users.noreply.github.com>
2020-04-16 10:35:12 +0700

    Update animate.c

--- animate.c
+++ animate.c
@@ -1,5 +1,5 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
- * MODUL 8 ¨C TUGAS BESAR
+ * MODUL 8 Â¨C TUGAS BESAR
  * Kelompok : 1
  * Hari dan Tanggal : Selasa, 14 April 2020
  * Asisten (NIM) : Berry (13216027)
@@ -18,6 +18,12 @@ void animate () {
 	int n,i;
 	printf("How many iterations do you want?\n");
 	scanf("%d", &n);
+	while (n<=0) {
+		printf("Iteration must be greater than 0!\n");
+		printf("Please input the right number of iteration\n");
+		printf("How many iterations do you want?\n");
+		scanf("%d", &n);
+	}
 	for (i=0; i<n; i++) {
 		//tick();				//import fungsi tick
 		//printArray();		//import fungsi print, dihapus kalo udah termasuk di tick
