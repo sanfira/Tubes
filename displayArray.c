55f953a5e26e5e563e2a3f865d76915095295c50
azinul <63405605+azinul@users.noreply.github.com>
2020-04-17 13:33:42 +0700

    Update displayArray.c

--- displayArray.c
+++ displayArray.c
@@ -18,7 +18,7 @@ void displayArray(unsigned char *GOL[], int baris, int kolom)
     {
         for(j=0;j<kolom;j++)
         {
-            printf("%c", GOL);
+            printf("%c", GOL[i][j]);
         }
     printf("\n");
     }
